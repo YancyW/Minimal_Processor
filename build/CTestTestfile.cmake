6ff2c2e25fb598752e0ec13fc77b335126aa73cc
YancyW <wang_yancy@outlook.com>
2018-10-10 12:04:09 +0200

    remove build folder and add some README

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /afs/desy.de/user/y/ywang/Code/Minimal_Processor
-# Build directory: /afs/desy.de/user/y/ywang/Code/Minimal_Processor/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
